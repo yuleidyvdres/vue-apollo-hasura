1e5d1dda4d46817b24fbc4aff49f1462ef0e95ef
Yuleidy Valladares <yv@plusteam.tech>
2020-02-05 14:44:45 -0400

    :apple: Add queries, mutations and subscriptions
    - Added queries and optimisticResponses to update the UI
    - Added mutations for one to many and many to many relationships
    - Added subscription
    - Created http and websocket links

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,60 +1,417 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex xs12>
+        <h1>Search User</h1>
+        <div v-if="$apollo.queries.user.loading"></div>
+        <div v-if="error">{{ error }}</div>
+        <div>Loading...</div>
+        {{ user }}
+      </v-flex>
+
+      <v-flex xs12>
+        <h1>Add user</h1>
+        <v-form>
+          <v-text-field v-model="newId" label="id"></v-text-field>
+          <v-text-field v-model="newUser" label="Name"></v-text-field>
+          <v-btn @click="addUser">Add</v-btn>
+        </v-form>
+      </v-flex>
+
+      <v-flex xs12>
+        <h1>Edit active status to user</h1>
+        <v-form>
+          <v-text-field v-model="userId" label="id"></v-text-field>
+          <v-checkbox v-model="isActive" label="Active"></v-checkbox>
+          <v-btn @click="updateUser">Update</v-btn>
+        </v-form>
+      </v-flex>
+
+      <v-flex xs12>
+        <h1>Add user and posts</h1>
+        <v-form>
+          <v-text-field v-model="userId" label="id"></v-text-field>
+          <v-text-field v-model="newUser" label="Name"></v-text-field>
+          <v-text-field v-model="postId" label="id"></v-text-field>
+          <v-text-field v-model="postTitle" label="Post title"></v-text-field>
+          <v-text-field v-model="postContent" label="Post content"></v-text-field>
+          <v-btn @click="addUserAndPosts">Add</v-btn>
+          <v-spacer></v-spacer>
+          {{ user }}
+        </v-form>
+      </v-flex>
+
+      <v-flex xs12>
+        <h1>Posts and tags</h1>
+        <v-btn @click="addPostAndTags">Add post and tags</v-btn>
+        <v-spacer></v-spacer>
+        {{ post }}
+      </v-flex>
+
+      <v-flex xs12>
+        <h1>Remove user</h1>
+        {{ users }}
+        <v-form>
+          <v-text-field v-model="userId" label="id"></v-text-field>
+          <v-btn @click="removeUser">Remove</v-btn>
+        </v-form>
+      </v-flex>
+
+      <v-flex xs12>
+        <h1>Subscription</h1>
+        <p>User updated: {{ suscribedUser }}</p>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+import gql from "graphql-tag";
+
+const ADD_USER = gql`
+  mutation ($id: Int, $name: String) {
+    insert_user(objects: {id: $id, name: $name}) {
+      affected_rows
+      returning {
+        id
+        name
+      }
   }
 }
-</script>
+`;
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+const UPDATE_ACTIVE_USER = gql`
+  mutation updateActiveUser($id: Int, $active: Boolean) {
+      update_user(where: { id: { _eq: $id } }, _set: { active: $active }) {
+          affected_rows
+      }
+  }
+`;
+
+const ADD_USER_AND_POSTS = gql`
+  mutation MyMutation($id: Int, $name: String, $posts: [post_insert_input!]!) {
+    insert_user(objects: {id: $id, name: $name, posts: {data: $posts}}) {
+        affected_rows
+        returning {
+            id
+            name
+            posts {
+                id
+                title
+                content
+            }
+        }
+    }
+  }
+`;
+
+const GET_USER_AND_POSTS = gql`
+  query {
+    user {
+      id
+      name
+      posts {
+        id
+        title
+        content
+      }
+    }
+  }
+`;
+
+const ADD_POST_AND_TAGS = gql`
+  mutation MyMutation($postId: Int, $postTitle: String, $postContent: String, $tag: [post_tag_insert_input!]!) {
+    insert_post(objects: {
+      id: $postId,
+      title: $postTitle,
+      content: $postContent,
+      user_id: 100,
+      post_tags: {
+        data: $tag
+      }
+    }) {
+      affected_rows
+      returning {
+        id
+        title
+        content
+        post_tags {
+          tag {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+const GET_POST_AND_TAGS = gql`
+  query MyQuery {
+    post {
+      id
+      title
+      content
+      post_tags {
+        tag {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+const REMOVE_USER = gql`
+  mutation MyMutation($id: Int) {
+    delete_user(where: {id: {_eq: $id}}) {
+      affected_rows
+    }
+  }
+`;
+
+const GET_USERS = gql`
+  query MyQuery {
+    users: user {
+      id
+      name
+    }
+  }
+`;
+
+const SUBSCRIPTION_NEW_USER = gql`
+  subscription MySubscription {
+    user(where: {id: {_eq: 19}}) {
+      id
+      name
+      active
+    }
+  }
+`;
+
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    newUser: null,
+    newId: null,
+    userId: null,
+    isActive: false,
+    error: null,
+    postId: null,
+    postTitle: null,
+    postContent: null,
+    suscribedUser: null,
+  }),
+  apollo: {
+    users: {
+      query: GET_USERS
+    },
+    user: {
+      query: GET_USER_AND_POSTS,
+      error(error) {
+        this.error = JSON.stringify(error.message);
+      }
+    },
+    post: {
+      query: GET_POST_AND_TAGS,
+      error(error) {
+        this.error = JSON.stringify(error.message);
+      }
+    },
+    $subscribe: {
+      user: {
+        query: SUBSCRIPTION_NEW_USER,
+        result (data) {
+          this.suscribedUser = data.data.user;
+        }
+      }
+    }
+  },
+  methods: {
+    addUser : function() {
+      this.$apollo.mutate({
+        mutation: ADD_USER,
+        variables: {
+          id: parseInt(this.newId),
+          name: this.newUser
+        },
+        update: (cache, { data: { insert_user } }) => {
+          try {
+            //Read query from cache
+            const data = cache.readQuery({
+              query: GET_USERS
+            });
+            //Get the response from mutation
+            const insertedUser = insert_user.returning;
+            //Insert the new user to the actual user's list
+            data.users.splice(0, 0, insertedUser[0]);
+            //Write the query to the cache
+            cache.writeQuery({
+              query: GET_USERS,
+              data
+            });
+            this.newId = null;
+            this.newUser = null;
+          } catch(e) {
+            console.error(e);
+          }
+        }
+      });
+    },
+    updateUser: function() {
+      const id = parseInt(this.userId);
+      this.$apollo.mutate({
+        mutation: UPDATE_ACTIVE_USER,
+        variables: {
+          id: parseInt(this.userId),
+          active: this.isActive
+        },
+        update: (store, { data: { update_user } }) => {
+          if (update_user.affected_rows) {
+            const data = store.readQuery({
+              query: GET_USERS
+            });
+            const activedUser = data.users.find(t => t.id === id);
+            activedUser.active = this.isActive;
+            store.writeQuery({
+              query: GET_USERS,
+              data
+            });
+          }
+        },
+        optimisticResponse: {
+          __typename: 'Mutation',
+          update_user: {
+            __typename: 'user',
+            id: id,
+            active: this.isActive,
+            affected_rows: 1
+          }
+        }
+      });
+    },
+    addUserAndPosts: function() {
+      this.$apollo.mutate({
+        mutation: ADD_USER_AND_POSTS,
+        variables: {
+          id: parseInt(this.userId),
+          name: this.newUser,
+          posts: [
+            {
+              id: parseInt(this.postId),
+              title: this.postTitle,
+              content: this.postContent
+            }
+          ]
+        },
+        update: (cache, { data: { insert_user } }) => {
+          try {
+            //Read query from cache
+            const data = cache.readQuery({
+              query: GET_USER_AND_POSTS
+            });
+            //Get the response from mutation
+            const insertedUser = insert_user.returning;
+            //Insert the new user to the actual user's list
+            data.users.splice(0, 0, insertedUser[0]);
+            //Write the query to the cache
+            cache.writeQuery({
+              query: GET_USER_AND_POSTS,
+              data
+            });
+            this.userId = null;
+            this.newUser = null;
+            this.postId = null;
+            this.postTitle = null;
+            this.postContent = null;
+          } catch(e) {
+            console.error(e);
+          }
+        }
+      });
+    },
+    addPostAndTags: function(){
+      this.$apollo.mutate({
+        mutation: ADD_POST_AND_TAGS,
+        variables: {
+          postId: 215,
+          postTitle: "TEST 3",
+          postContent: "I don't know",
+          tag: [
+            {
+              tag: {
+                data: {
+                  id: 1,
+                  name: "Info"
+                },
+                on_conflict: {
+                  "constraint": "tag_pk",
+                  "update_columns": "name"
+                }
+              }
+            },
+            {
+              tag: {
+                data: {
+                  id: 100,
+                  name: "Entertainment"
+                },
+                on_conflict: {
+                  constraint: "tag_pk",
+                  update_columns: "name"
+                }
+              }
+            }
+          ]
+        },
+        update: (cache, { data: { insert_post } }) => {
+          try {
+            //Read query from cache
+            const data = cache.readQuery({
+              query: GET_POST_AND_TAGS
+            });
+            //Get the response from mutation
+            const insertedPost = insert_post.returning;
+            //Insert the new user to the actual user's list
+            data.post.splice(0, 0, insertedPost[0]);
+            //Write the query to the cache
+            cache.writeQuery({
+              query: GET_POST_AND_TAGS,
+              data
+            });
+          } catch(e) {
+            console.error(e);
+          }
+        }
+      });
+    },
+    removeUser: function() {
+      this.$apollo.mutate({
+        mutation: REMOVE_USER,
+        variables: {
+          id: parseInt(this.userId)
+        },
+        update: (store, { data: { delete_user } }) => {
+          if (delete_user.affected_rows) {
+            const data = store.readQuery({
+              query: GET_USERS
+            });
+            data.users = data.users.filter(t => {
+              return t.id !== parseInt(this.userId);
+            });
+            store.writeQuery({
+              query: GET_USERS,
+              data
+            });
+          }
+        }
+      })
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,77 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+import { ApolloClient } from 'apollo-client';
+import { WebSocketLink } from 'apollo-link-ws';
+import { createHttpLink } from "apollo-link-http";
+import { split } from "apollo-link";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
+import { getMainDefinition } from 'apollo-utilities';
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:8080/v1/graphql',
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:8080/v1/graphql',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      headers: {
+        "x-hasura-admin-secret": "edv"
+      }
+    }
+  }
+});
+
+const link = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+          definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+      );
+    },
+    wsLink,
+    httpLink
+);
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      "x-hasura-admin-secret": "edv"
+    }
+  }
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  //link: authLink.concat(httpLink),
+  link: authLink.concat(link),
+  cache,
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+
+Vue.config.productionTip = false;
+Vue.use(VueApollo);
 
 new Vue({
   router,
   store,
+  vuetify,
+  apolloProvider,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
